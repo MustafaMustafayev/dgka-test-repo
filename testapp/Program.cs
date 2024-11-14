1c77d850dae6fe04abd1b07c7474d3eab2a091db
mustafa <mmustafayev0001@gmail.com>
2024-11-14 09:17:09 +0400

    added admin login

---
+++ testapp/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace testapp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter username: ");
+        string username = Console.ReadLine();
+
+        Console.WriteLine("enter password: ");  
+        string password = Console.ReadLine();
+
+        bool isAdmin = Program.LoginAdmin(username, password);
+
+        if(isAdmin)
+        {
+            Console.WriteLine("logged in");
+        }
+        else
+        {
+            Console.WriteLine("can not log in");
+        }
+    }
+
+    static bool LoginAdmin(string name, string password)
+    {
+        if(name == "admin" && password == "admin")
+        {
+            return true;
+        }
+        return false;
+    }
+}
